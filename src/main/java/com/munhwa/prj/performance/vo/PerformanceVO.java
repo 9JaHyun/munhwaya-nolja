c3e8b190661eeb8b1970c2f2d023701313bc8162
Kanggyengbin <94347489+Kanggyengbin@users.noreply.github.com>
2022-03-30 14:59:13 +0900

    Update PerformanceVO.java

--- src/main/java/com/munhwa/prj/performance/vo/PerformanceVO.java
+++ src/main/java/com/munhwa/prj/performance/vo/PerformanceVO.java
@@ -1,6 +1,5 @@
 package com.munhwa.prj.performance.vo;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,12 +13,8 @@ public class PerformanceVO {
 	private int id;
 	private String name;
 	private String location;
-	
-	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-	private LocalDateTime sdate;
-	
-	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-	private LocalDateTime edate;
+	private Date sdate;
+	private Date edate;
 	private String content;
 	private String status;
 	private String image;
